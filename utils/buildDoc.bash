5febe145b289dfad652646fa0bbff22c51489ade
Laurent MICHEL <laurent.michel@astro.unistra.fr>
2024-10-03 16:57:10 +0200

    check mivot-validator is installed: continue anyway

--- utils/buildDoc.bash
+++ utils/buildDoc.bash
@@ -4,11 +4,17 @@ echo "build VODML"
 python -m processVodml
 
 echo "build MIVOT snippets"
-echo "do not forget 'pip install mivot-validator'"
 [ -e ../mivot/mango ] && rm -rf ../mivot/mango
-cp ../vo-dml/desc.mango.vo-dml.xml ./mango.vo-dml.xml
-mivot-snippet-model file://`pwd`/mango.vo-dml.xml `pwd`/../mivot/
 
+which mivot-snippet-model
+if [[ $? -eq 0 ]]; then
+	cp ../vo-dml/desc.mango.vo-dml.xml ./mango.vo-dml.xml
+	mivot-snippet-model file://`pwd`/mango.vo-dml.xml `pwd`/../mivot/
+	rm ./mango.vo-dml.xml
+else
+	echo "mivot-snippet-model not found: have you 'pip install mivot-validator'"
+fi
+exit 1
 cd ../doc
 echo "compile latex"
 rm *.aux
