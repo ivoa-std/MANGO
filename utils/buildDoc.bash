ae05bc1deb9b353a277764b4709d4fea17c1da6d
Laurent MICHEL <laurent.michel@astro.unistra.fr>
2024-10-03 16:51:40 +0200

    MIVOT snippets generated by buildDoc

--- utils/buildDoc.bash
+++ utils/buildDoc.bash
@@ -6,8 +6,8 @@ python -m processVodml
 echo "build MIVOT snippets"
 echo "do not forget 'pip install mivot-validator'"
 [ -e ../mivot/mango ] && rm -rf ../mivot/mango
-mivot-snippet-model file://`pwd`/../vo-dml/desc.mango.vo-dml.xml `pwd`/../mivot/
-mv ../mivot/desc ../mivot/mango
+cp ../vo-dml/desc.mango.vo-dml.xml ./mango.vo-dml.xml
+mivot-snippet-model file://`pwd`/mango.vo-dml.xml `pwd`/../mivot/
 
 cd ../doc
 echo "compile latex"
