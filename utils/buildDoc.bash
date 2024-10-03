0deb70c71db8996b5ecfecf9a503292ffb00ea84
Laurent MICHEL <laurent.michel@astro.unistra.fr>
2024-10-03 17:47:05 +0200

    generate mivot snippets at build time

--- utils/buildDoc.bash
+++ utils/buildDoc.bash
@@ -4,17 +4,17 @@ echo "build VODML"
 python -m processVodml
 
 echo "build MIVOT snippets"
-[ -e ../mivot/mango ] && rm -rf ../mivot/mango
 
 which mivot-snippet-model
 if [[ $? -eq 0 ]]; then
+    [ -e ../mivot/mango ] && rm -rf ../mivot/mango
 	cp ../vo-dml/desc.mango.vo-dml.xml ./mango.vo-dml.xml
 	mivot-snippet-model file://`pwd`/mango.vo-dml.xml `pwd`/../mivot/
 	rm ./mango.vo-dml.xml
 else
 	echo "mivot-snippet-model not found: have you 'pip install mivot-validator'"
 fi
-exit 1
+
 cd ../doc
 echo "compile latex"
 rm *.aux
